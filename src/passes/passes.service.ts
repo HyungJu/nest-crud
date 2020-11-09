9d84720ca4fd4d2ac346fd60bcab3e824d876209
HyungJu <sungkisa@naver.com>
2020-11-09 19:50:25 +0900

    feat: return ResponseDtos rather than RequestDto

--- src/passes/passes.service.ts
+++ src/passes/passes.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import {BarcodeType, Pass} from './passes.entity';
-import {InsertResult, Repository, UpdateResult} from 'typeorm';
-import {CreatePassDto} from "./dto/create-pass.dto";
-import {UpdatePassDto} from "./dto/update-pass.dto";
+import { BarcodeType, Pass } from './passes.entity';
+import { InsertResult, Repository, UpdateResult } from 'typeorm';
+import { CreatePassDto } from './dto/create-pass.dto';
+import { UpdatePassDto, UpdatePassResponseDto } from './dto/update-pass.dto';
 
 @Injectable()
 export class PassesService {
@@ -24,23 +24,37 @@ export class PassesService {
     await this.passRepository.delete(uuid);
   }
 
-  async update(uuid: string, pass: UpdatePassDto): Promise<UpdatePassDto> {
+  async update(
+    uuid: string,
+    pass: UpdatePassDto,
+  ): Promise<UpdatePassResponseDto> {
     const passToUpdate = await this.find(uuid);
     passToUpdate.code = pass.code;
     passToUpdate.name = pass.name;
     passToUpdate.type = BarcodeType[pass.type];
 
-    const ret = await this.passRepository.update(uuid, passToUpdate);
-    return pass;
+    await this.passRepository.update(uuid, passToUpdate);
+
+    return {
+      code: pass.code,
+      name: pass.name,
+      type: pass.type,
+      id: uuid,
+    };
   }
 
-  async insert(pass: CreatePassDto): Promise<CreatePassDto> {
+  async insert(pass: CreatePassDto): Promise<UpdatePassResponseDto> {
     const passCreated = new Pass();
     passCreated.code = pass.code;
     passCreated.name = pass.name;
     passCreated.type = BarcodeType[pass.type];
 
     const ret = await this.passRepository.insert(passCreated);
-    return pass;
+    return {
+      code: pass.code,
+      name: pass.name,
+      type: pass.type,
+      id: ret.identifiers[0].id,
+    };
   }
 }
